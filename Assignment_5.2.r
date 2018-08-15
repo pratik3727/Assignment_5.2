8d8febecc7057f88c4cb142d8a20a520623b4041
pratik3727 <39432082+pratik3727@users.noreply.github.com>
2018-08-16 00:42:29 +0530

    Assignment_5.2
    Solution for assignment_5.2

---
+++ Assignment_5.2.r
@@ -0,0 +1,19 @@
+
+#Assignment 5.2
+
+#1. P value in problem 2
+# If alpha value is 5%, then Zscore is less then -1.96 or greater than 1.96
+X<- pnorm(1.96, 0, 1)
+x
+
+
+#2.How do you test the proportions and compare against hypothetical props? Test Hypothesis: proportion of automatic cars is 40%.
+
+#Null hypothesis is proportion of automatic cars is 40%
+
+#Alternate hypothesis is proportion of automatic cars is not 40%
+
+
+#To test proportion we need to find Z value , also mean and standard deviation 
+
+
